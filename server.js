f896351628bd62d567e58f49eb432f5807556224
Piloto.Rafael.s314418 <s314418@rrisd.local>
2018-05-10 11:22:17 -0500

    Added all directories

--- server.js
+++ server.js
@@ -9,6 +9,43 @@ app.get("/language", function(req, res){
     res.send("language/")
 })
 
+app.get("/music", function(req, res){
+    res.send("music/")
+})
+
+app.get("/religion", function(req, res){
+    res.send("religion/")
+})
+
+app.get("/music", function(req, res){
+    res.send("music/")
+})
+
+app.get("/about", function(req, res){
+    res.send("about/")
+})
+
+app.get("/symbol", function(req, res){
+    res.send("symbol/")
+})
+
+
+app.get("/economy", function(req, res){
+    res.send("economy/")
+})
+
+app.get("/politics", function(req, res){
+    res.send("politics/")
+})
+
+app.get("/education", function(req, res){
+    res.send("education/")
+})
+
+app.get("/sport", function(req, res){
+    res.send("sport/")
+})
+
 let server = app.listen(port, () => {
     console.log("Example app listening at http://" + server.address().address + ":" + server.address().port)
 
